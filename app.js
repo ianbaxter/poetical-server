3eb257052e8c6c33f023deb65b075bbb872b38f4
Ian <41586905+ianbaxter@users.noreply.github.com>
2019-10-25 21:15:53 +0100

    Add compression and helmet

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const compression = require("compression");
+const helmet = require("helmet");
 const app = express();
 const checkAuth = require("./routes/api/checkAuth");
 dotenv.config();
@@ -24,6 +26,8 @@ app.use(cors({ origin: true, credentials: true }));
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 
 // Use routes
 app.use("/api/blogHome", blogHomeRouter);
