9128c46349bbef00596c93de21348e3cdab21ec0
Ian <41586905+ianbaxter@users.noreply.github.com>
2019-11-01 16:28:36 +0000

    Added a Procfile.

--- app.js
+++ app.js
@@ -37,5 +37,8 @@ app.get("/api/secret", checkAuth, (req, res) =>
 );
 
 // Start server
-const PORT = 8000 || process.env.PORT;
-app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, () => console.log(`Server listening on ${port}`));
